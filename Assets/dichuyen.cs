aa9aba50df7d9ceba05e8d21e7586148b664a5bd
linhbh02 <linhbhce160280@fpt.edu.vn>
2023-10-04 11:49:19 +0700

    init project

---
+++ Assets/dichuyen.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dichuyen : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Animator anim;
+    public int tocDo = 4;
+    public float traiPhai;
+    public bool isFacingRight = true;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        traiPhai = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(tocDo * traiPhai, rb.velocity.y);
+
+        if (isFacingRight == true && traiPhai == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            isFacingRight = false;
+        }
+        if (isFacingRight == false && traiPhai == 1)
+        {
+
+            transform.localScale = new Vector3(1, 1, 1);
+            isFacingRight = true;
+        }
+        //Animation
+        anim.SetFloat("dichuyen", Mathf.Abs(traiPhai));
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("tancong");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("dead"); 
+        }
+    }
+}
